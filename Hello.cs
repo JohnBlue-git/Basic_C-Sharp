ecc55313d8ed371b70ada7c664910aa31032ae58
JohnBlue-git <83207141+JohnBlue-git@users.noreply.github.com>
2022-04-29 09:30:13 +0800

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,24 @@
+﻿/*
+Auther: John Blue
+Time: 2022/5
+Platform: VS2017
+Object: say Hello World
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();// 等候使用者先按下Enter 鍵，再終止應用程式
+        }
+    }
+}
